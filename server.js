2d0dab89cdc20f5b1c4a4679252c78e67451b0c7
LeeDoHyun <wonehgus1234@naver.com>
2024-02-21 13:27:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ var browser
 async function scrapeData(url,type) {
   try {
     browser = await puppeteer.launch({
-    executablePath: '/usr/bin/google-chrome',
+    executablePath: '/usr/bin/chromium-browser',
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
